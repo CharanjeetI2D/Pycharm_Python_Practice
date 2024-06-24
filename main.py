90f58179d5877bc3b0599d47427c2751328f0e99
CharanjeetI2D <charanjeet8149@gmail.com>
2024-06-24 18:03:58 +0530

    Read_write_Files

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
+file_path = '/Users/charanjeetsingh/Documents/PythonProjects/Python_ Experiments/Todos.txt'
+
 while True:
     todo_action = input('Type operation: add, show, edit, exit, remove : ').strip()
 
     match todo_action:
-
+        #
         case 'add':
             new_item = input("Please enter value to add: ").capitalize() + "\n"
 
-            file = open('Todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # Reading the file
+            with open(file_path, 'r') as file:
+                todos = file.readlines()
 
+            # adding the new item to todos list
             todos.append(new_item)
 
-            file = open('Todos.txt', 'w')   # Create a file object to write to file
-            file.writelines(todos)
-            file.close()
-
+            # Create a file object to write to file
+            with open(file_path, 'w') as file:
+                file.writelines(todos)
 
         case 'display' | 'show':
 
-            read_file = open('Todos.txt', 'r')
-            todos = read_file.readlines()
-            read_file.seek(0)
+            with open(file_path, 'r') as read_file:
+                todos = read_file.readlines()
+
+            # new_todos = []
+            # for items in todos:
+            #     new_item = items.strip('\n')
+            #     new_todos.append(new_item)
+
+            #List comprehension
+           # new_todos = [items.strip('\n') for items in todos]
 
             for index, items in enumerate(todos):
-                all_items = f"{index + 1} {items}"
+                item = items.strip('\n')
+                all_items = f"{index + 1}-{item}"
                 print(all_items)
 
         case 'edit':
-            print("Below are all the items")
-            for item_index, items in enumerate(todos):
-                row = f"{item_index + 1}-{items}"
-                print(row)
-            item_index = int(input("Which item number would want to edit from above?: "))
-            print(f"You have selected {todos[item_index - 1]} to change")
-            new_item = input("Enter a new item: ")
-            todos[item_index - 1] = new_item
+            with open(file_path, 'r') as read_file:
+                todos = read_file.readlines()
+            print(todos)
+
+            user_input = int(input("Which item number would want to edit from above?: "))
+            index_position = user_input - 1
+
+            print(f"You have selected {todos[index_position]} to change")
+
+            new_todo = input("Enter a new item: ")
+            todos[index_position] = new_todo.title() + '\n'
             print("Item edit success")
             print(todos)
 
-        case 'complete' | 'remove':
-            for item_index, item in enumerate(todos):
-                item_string = f"{item_index + 1} : {item}"
-                print(item_string)
+            with open(file_path, 'w') as file:
+                file.writelines(todos)
+
+        case 'remove':
+            # Take the input form the user
             user_input = int(input("Please enter item number you want to remove: "))
-            todos.pop(user_input - 1)
-            print(f"New list: ", todos)
+
+            with open(file_path, 'r') as read_file:
+                read_file.readlines()
+
+            index_position = user_input - 1
+            todo_to_remove = todos[index_position].strip()
+
+            todos.pop(index_position)
+
+            # save the new to dos in a new file
+            with open(file_path, 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
+
 
         case 'exit' | 'quit':
             break
